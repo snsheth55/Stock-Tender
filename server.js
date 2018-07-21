2d9894420368fd4e22eea2b11f9557a60f03b6d9
budken <siu.afmm@gmail.com>
2018-07-21 09:56:15 -0400

    Update - createUser, created database, combined files

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
-const eventController = require('./database/server/eventController');
+const body = require('body-parser');
+const main = require('./database/data/postgreSQL-main.js');
+// const eventController = require('./database/server/event-controller-model.js');
+
+app.use(body.json(), body.urlencoded({ extended: true }));
 
 //** login get user information
-app.get('/login', eventController.getUser);
+app.get('/', (req, res) => {
+  console.log('Main page connected');
+});
+app.get('/login', (req, res) => {});
+
+app.post('/createUser', main.createUser);
 
 //**  add items to stock
-app.post('/addStock', eventController.addItem);
+app.post('/addStock', () => {});
 
 //** get stock table
-app.get('/viewStock', eventController.showAllStock);
+app.get('/viewStock', () => {});
 
 //** edit individual stock items
-app.post('/editStock', eventController.editItem);
+app.post('/editStock', () => {});
 
 //** remove record of item in stock
-app.post('/deleteItem', eventController.deleteItem);
+app.post('/deleteItem', () => {});
 
 //**
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Listening to port 3000');
+});
 
 module.exports = app;
