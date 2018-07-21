5c695e0a962acbeab5efa008868e3a825b49c54a
jmiles90 <jmilescespedes@gmail.com>
2018-07-20 20:54:29 -0400

    content component created

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import Nav from './Nav.js';
+import Content from './Content.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <Content />
       </div>
     );
   }
